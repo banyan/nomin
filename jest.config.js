c4b51a5875e95686d6932aaf1be634c65f34281e
Kohei Hasegawa <ameutau@gmail.com>
2020-04-23 00:00:29 +0900

    Add testPathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     "json",
     "node",
   ],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
