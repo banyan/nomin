8c9e34d682aafaf4127436a92e4a7c8aa3c5363b
Kohei Hasegawa <ameutau@gmail.com>
2020-04-25 17:35:43 +0900

    Extract createPost in commands/new

---
+++ src/commands/__tests__/new.test.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import { advanceTo, clear } from 'jest-date-mock';
+
+import { paths } from '~/paths';
+import { createPost } from '~/commands/new';
+
+describe('createPost', () => {
+  const spy = jest.spyOn(fs.promises, 'writeFile');
+  advanceTo(new Date(2020, 3, 25, 0, 0, 0)); // 3 is actually 4
+
+  afterAll(() => {
+    clear();
+  });
+
+  it('creates new post', async () => {
+    const title = 'foo'
+    await createPost(title);
+    expect(spy).toHaveBeenCalled();
+    expect(spy.mock.calls[0][0]).toEqual(`${paths.posts}/2020-04-25-${title}.md`);
+    expect(spy.mock.calls[0][1]).toMatchInlineSnapshot(`
+      "---
+      title: foo
+      date: 2020-04-25T00:00:00+09:00
+      ---"
+    `);
+  });
+});
---
+++ src/commands/new.ts
@@ -0,0 +1,19 @@
+
+import fs from 'fs';
+import { format, formatRFC3339 } from 'date-fns';
+
+import { paths } from '~/paths';
+
+const fsp = fs.promises;
+
+export const createPost = async (title: string): Promise<void> => {
+  try {
+    const time = new Date();
+    await fsp.writeFile(
+      `${paths.posts}/${format(time, 'yyyy-MM-dd')}-${title}.md`,
+      `---\ntitle: ${title}\ndate: ${formatRFC3339(time)}\n---`,
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
